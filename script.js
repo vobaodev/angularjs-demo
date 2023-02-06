806bbe9008ccd9f67c747e96c784157ab6ec8e0e
vobaodev <votranduybao.work@gmail.com>
2023-02-06 14:41:18 +0700

    Modified

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 (function () {
-    angular.module("myFirstApplication", []).controller("myFirstController", function ($scope) {
-        $scope.name = "Vo Bao";
-    });
-});
+    'use strict';
+    angular.module('nameCalculatorApp', [])
+        .controller('nameCalculatorController', function ($scope) {
+            $scope.name = '';
+            $scope.totalValue = 0;
+            $scope.calculate = function () {
+                var totalNameValue = calculateNumericForString($scope.name);//get total value;
+                $scope.totalValue = totalNameValue;
+            };
+        });
+    function calculateNumericForString(string) {
+        var totalLength = 0;
+        for (var i = 0; i < string.length; i++) {
+            totalLength += string.charCodeAt(i);
+        }
+        return totalLength;
+    }
+})();
