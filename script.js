48dcf73464e069e3deb0aef5961a484a57aa6b8e
vobaodev <votranduybao.work@gmail.com>
2023-02-06 11:22:57 +0700

    Change for project angularjs-demo

---
+++ script.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    angular.module('myFistApp', [])
+        .controller('myFirstController', function ($scope) {
+            @$scope.name = "Bao Vo";
+        });
+})();
